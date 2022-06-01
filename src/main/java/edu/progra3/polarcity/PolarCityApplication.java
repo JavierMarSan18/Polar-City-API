9aedb4716c9404c4ce30ad9c502466b40a730d02
JavierMarSan18 <marroquinjavier75@gmail.com>
2022-06-01 16:50:38 -0600

    Prueba heroku

---
+++ src/main/java/edu/progra3/polarcity/PolarCityApplication.java
@@ -0,0 +1,13 @@
+package edu.progra3.polarcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/progra3/polarcity/PolarCityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.progra3.polarcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
